581329121e7fd3583f980fc34f58d533c888d9ed
xingsu1021 <xingsu1021@sina.com>
2022-05-23 14:08:49 +0800

    新增nginx unit

--- start.sh
+++ start.sh
@@ -2,7 +2,6 @@
 
 #初始化数据库
 python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:80
-#gunicorn pthelper.wsgi:application --bind 0.0.0.0:8000 -w 4 --threads 2 --daemon
+#python3 manage.py runserver 0.0.0.0:80
 
-#nginx -g 'daemon off;'
+/usr/sbin/unitd --no-daemon --control 0.0.0.0:60081 --user root --group root
